0bd298210725b43a185bb288f8c4bce09b021465
albertsalas <albsalas@csumb.edu>
2020-09-28 21:17:30 -0700

    finished creating database

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// database setup
+const connnection = mysql.createConnection({
+        host: process.env.JAWSDB_MARIA_HOST,
+        user: process.env.JAWSDB_MARIA_USER,
+        password: process.env.JAWSDB_MARIA_PASSWORD,
+        database: process.env.JAWSDB_MARIA_DATABASE,
+    }
+);
+console.log("database created!");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +26,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +34,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
