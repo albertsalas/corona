4bf9b15ba5c391812d9b1c05c1a6148bcca226a1
OzzieM <ozmoreno@csumb.edu>
2020-10-21 21:54:05 -0700

    it won't work :(

--- routes/shoppingcart.js
+++ routes/shoppingcart.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const products = require("../controllers/ProductsController.js");
+const sc = require("../controllers/searchController.js");
 
 
 router.get('/', function(req,res, next) {
   res.render('ShoppingCart', {title:'Shopping Cart'});
 });
+
+router.patch('/:name', sc.update);
 
 module.exports = router;
