3c843ca86034aa739dc718539b98409f2d4b2043
OzzieM <ozmoreno@csumb.edu>
2020-10-23 21:00:35 -0700

    made an orders page

--- routes/OrdersRouter.js
+++ routes/OrdersRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const orders = require("../controllers/OrdersController.js");
 
 router.post('/', orders.create);
+router.get('/', function(req,res, next) {
+    res.render('orders', {title:'Orders'});
+});
 
 module.exports = router;
